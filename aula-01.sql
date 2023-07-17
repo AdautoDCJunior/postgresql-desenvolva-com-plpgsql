f5d1d8434af8f91ad71ee817813ea31590aae473
Adauto Júnior <dev.adautojunior@gmail.com>
2023-07-17 16:19:38 -0300

    fix: adding missing class material

--- aula-01.sql
+++ aula-01.sql
@@ -3,3 +3,26 @@ create function primeira_funcao() returns integer as '
 ' language sql;
 
 select primeira_funcao() as numero;
+
+create function soma_dois_numeros(
+	numero_1 integer,
+	numero_2 integer
+) returns integer as '
+	select numero_1 + numero_2
+' language sql;
+
+select soma_dois_numeros(4, 4) as numero;
+
+create temp table if not exists a (
+	id serial primary key,
+	nome varchar(255)
+);
+
+create or replace function cria_a(nome varchar) returns void as $$
+	insert into a (nome) values (cria_a.nome);
+$$ language sql;
+
+select cria_a('Teste');
+
+select * from a;
+
